daa7564f523c1f567d63e16c8b0e886400909434
PeaWagon <jgarner.py@gmail.com>
2017-02-27 17:54:16 -0500

    added input/output NOTE: not stable atm. Need to add fields for reading in special characters

--- DataSheet_main.py
+++ DataSheet_main.py
@@ -2,6 +2,14 @@
 
 # simulation class to instantiate and update simulation times
 
+
+# TO DO
+
+# make sure no fields contain '
+# this will cause issues for 
+# fun shit happens with random characters inserted. need to fix
+
+
 import os
 
 class DataSheet(object):
@@ -9,8 +17,8 @@ class DataSheet(object):
     def __init__(self):
         self.fname = self.get_filename()
         self.num_simulations = self.count_sim()
-        self.simulation_names = self.get_sim_names()
         self.main_dict = self.read_datafile()
+        self.simulation_names = self.get_sim_names()
     
     def get_filename(self):
         """ the user will be prompted for a name to same simulation
@@ -27,26 +35,15 @@ class DataSheet(object):
             print("Data will be stored in "+fname+'.')
             return fname
     
-    def write_header(self):
-        """ writes header to file 
-            called if file does not exist (self.count_sim())
-            or if file is to be updated with new information
-        """ 
-        with open(self.fname, 'w') as f:
-            f.write("'Simulation Name', 'WT/Mu', 'Protein Name', 'Bacteria Name', 'Residue Range', 'Salt Type', 'Salt Concentration', 'Extra Comments', 'Progress (ns)', 'Server Name', 'Source Directory'\n")
-    
-    
     def count_sim(self):
-        """ 
-            
+        """ determines how many simulations currently exist
+            in the fname given
         """
-        # if fname does not exist, fill with header, return 0 simulations
+        # if fname does not exist, return 0 simulations
         if os.path.isfile(self.fname) == False:
-            self.write_header()
             return 0
-        # check if file is empty, fill with header
+        # check if file is empty
         elif os.path.getsize(self.fname) == 0:
-            self.write_header()
             return 0
         # if not empty, ignore header, count the number of simulations    
         else:
@@ -62,29 +59,63 @@ class DataSheet(object):
             return counter
 
     def get_sim_names(self):
-        if os.path.isfile(self.fname) == False or self.num_simulations == 0:
+        """ returns empty list if there is no data in fname
+            needs to be updated AFTER the dictionary has been updated
+            as it uses the dictionary to pull out simulation names
+        """
+        if self.count_sim == 0:
             print("No data has been written yet to "+self.fname+'.')
             return []
         else:
-            print("Data will now be read from file "+fname+'.')
-            simulation_names = []
-            with open(self.fname, 'r') as f:
-                for line in f:
-                    simulation_names.append(line[:line.index(',')])
-            self.simulation_names = sorted(simulation_names)
+            print("Data will now be read from file "+self.fname+'.')
+            self.simulation_names = []
+            for key in sorted(self.main_dict):
+                self.simulation_names.append(key)
             return self.simulation_names
     
     def read_datafile(self):
         """ pulls all data from the fname file and stores it
             in a dictionary
         """
+        # avoids trying to open non-existent file
         if self.count_sim() == 0:
             return {}
-        else:
-            self.main_dict = {}
-            with open(self.fname, 'r') as f:
-                for line in self.fname:
-                    self.main_dict[line[:line.index(',')]] = line
+        
+        header = True
+        self.main_dict = {}
+        with open(self.fname, 'r') as f:
+            for line in f:
+                if header == True:
+                    header = False
+                else:
+                    sim_name = line[1:line.index(',')-1]  
+                    values = line[:-1].split(', ')
+                    # gross formatting crap
+                    # text is indicated by ' ' 
+                    # may have commas within text
+                    data = []
+                    start = True
+                    entry = ''
+                    add_entry = False
+                    for item in values:
+                        if item.startswith("'") and item.endswith("'"):
+                            data.append(item[1:-1])
+                        elif start == True and item.startswith("'"):
+                            entry+=item
+                            start = False
+                            add_entry = True
+                        elif start == False and add_entry == True and item.endswith("'"):
+                            entry+=item
+                            data.append(entry[1:-1])
+                            start = True
+                            add_entry = False
+                        elif start == False and add_entry == True:
+                            entry+=item
+                        else:
+                            data.append(float(item))
+              
+                    
+                    self.main_dict[sim_name] = data
             return self.main_dict
                     
                 
@@ -97,12 +128,14 @@ class DataSheet(object):
             return
         else:
             print(str(self.num_simulations)+" simulations found in "+self.fname+'.')
+            print()
             print("          Simulation Name                 | Number ")
             print("------------------------------------------|--------") 
             for i, name in enumerate(self.simulation_names):
                 # : is for padding. ^ means centre, > means right justified
                 # left justified is default
                 print("{:42}|{:^8}".format(name, i))            
+            print()
             return
 
 
@@ -124,6 +157,21 @@ class DataSheet(object):
                 else:
                     return sim_num
 
+    def write_data(self):
+        """ this function writes the contents of the main dictionary
+            self.main_dict to a file, called fname
+            the first instance of each simulation (the sim_name) is
+            used to sort the keys and hense the file is written
+            in alphabetical order according to sim_name
+        """
+        with open(self.fname, 'w') as f:
+            # write header
+            f.write("'Simulation Name', 'WT/Mu', 'Protein Name', 'Bacteria Name', 'Residue Range', 'Salt Type', 'Salt Concentration', 'Extra Comments', 'Progress (ns)', 'Server Name', 'Source Directory'\n")
+            for key in sorted(self.main_dict):
+                sim = self.main_dict[key][:]    # make copy of dict item
+                sim = str(sim).strip('[""]')+'\n' # rm sq brackets, add newline
+                f.write(sim)
+    
     def add_sim(self):
         """ gets information from user about a new simulation
             the data will be added to the file
@@ -133,7 +181,67 @@ class DataSheet(object):
         print()
         
         new_sim = []
+        new_sim.append(self.get_sim_name())
+        if new_sim[0] == 'q':
+            return 'q'
+        new_sim.append(self.get_sim_type())
+        if new_sim[1] == 'q':
+            return 'q'
+        new_sim.append(self.get_protein_name())
+        if new_sim[2] == 'q':
+            return 'q'
+        new_sim.append(self.get_protein_bacteria())
+        if new_sim[3] == 'q':
+            return 'q'
+        new_sim.append(self.get_res_range())
+        if new_sim[4] == 'q':
+            return 'q'
+        new_sim.append(self.get_salt_name())
+        if new_sim[5] == 'q':
+            return 'q'
+        new_sim.append(self.get_salt_conc())
+        if new_sim[6] == 'q':
+            return 'q'
+        new_sim.append(self.get_comments())
+        if new_sim[7] == 'q':
+            return 'q'                  
+        new_sim.append(self.get_sim_length())
+        if new_sim[8] == 'q':
+            return 'q'
+        new_sim.append(self.get_sim_server())
+        if new_sim[9] == 'q':
+            return 'q'
+        new_sim.append(self.get_sim_dir())
+        if new_sim[10] == 'q':
+            return 'q'
         
+        print('', new_sim, '', "Add this simulation to "+self.fname+"?", sep='\n')
+        # confirm addition:
+        while True:
+            ans = input("Y/n ") 
+            if ans in ('yes', 'YES', 'Yes', 'Y', 'y'):
+                print("Simulation will be added to "+self.fname+'.')
+                break
+            elif ans in ('no', 'NO', 'No', 'N', 'n', 'q'):
+                print("Simulation was not added to the database.")
+                return 'q'
+            else:
+                print("Invalid input. Try again.")
+        
+        # add to database
+        self.simulation_names.append(new_sim[0])
+        self.main_dict[new_sim[0]] = new_sim
+        self.num_simulations += 1
+        
+        # re-write file
+        self.write_data()
+        
+            
+    def get_sim_name(self):        
+        """ returns the name of the simulation or q for qutting/going 
+            up a level in the nested loops. User cannot input a name
+            if it already exists in the data file.
+        """
         while True:
             print("Enter the name of the new simulation.")
             sim_name = input()
@@ -142,9 +250,12 @@ class DataSheet(object):
             elif sim_name in self.simulation_names:
                 print("That simulation name already exists. Please enter a new one.", '', sep='\n')
             else:
-                new_sim.append(sim_name)
-                break
-                
+                return sim_name
+    
+    def get_sim_type(self):
+        """ the user provides information as to whether the simulation
+            is of a wild-type peptide or mutant peptide
+        """                
         while True:
             print("Is simulation of a wild-type (1) or mutant (2) peptide?")
             p_type = input()
@@ -154,33 +265,46 @@ class DataSheet(object):
                 print("Invalid input. Please try again.")
             elif p_type == '1':
                 p_type = 'WT'
-                new_sim.append(p_type)
-                break
+                return p_type
             elif p_type == '2':
                 mutation = input("Enter the mutation (ex E480C): ")
                 p_type = 'Mu_'+mutation
-                new_sim.append(p_type)
-                break
+                return p_type
 
+    def get_protein_name(self):
+        """ user provides name of protein for peptide being simulated
+        """       
         print("Provide the name of the protein being simulated (ex ProP):")
         protein = input()
         if protein == 'q':
             return 'q'
         else:
-            new_sim.append(protein)
-      
+            return protein
+
+    def get_protein_bacteria(self):
+        """ user provides name of bacteria that protein is from
+        """      
         print("What bacteria name is your protein from (ex Ec, Xc)?")
         bacteria = input()
         if bacteria == 'q':
             return 'q'
         else:
-            new_sim.append(bacteria)
-        
+            return bacteria
+
+    def get_res_range(self):
+        """ user provides first residue being simulated, followed
+            by the last residue
+            first number has to be a non-zero, positive integer
+            second integer has to be higher/equal to than the first, 
+            and a non-zero, positive integer
+        """        
         while True:
             print("For the protein being simulated, what is the first residue number?")
             res_start = input()
             if res_start == 'q':
                 return 'q'
+            # note that trying to convert the string to a integer directly
+            # will NOT work if the string is a floating point number! 
             try:
                 res_start = int(res_start)
             except ValueError:
@@ -205,24 +329,29 @@ class DataSheet(object):
                     print("The last residue number should be greater than the first residue number. Please try again.")
                 else:
                     residue_range = str(res_start)+'-'+str(res_finish)
-                    new_sim.append(residue_range)
-                    break
+                    return residue_range
         
+    def get_salt_name(self):
+        """ user provides salt type for simulation
+        """        
         print("Enter the salt type used in the simulation. Use \"N/A\" if no salt was used.")
         salt_type = input()
         if salt_type == 'q':
             return 'q'
         else:
-            new_sim.append(salt_type)
-            
+            return salt_type
+
+    def get_salt_conc(self):    
+        """ user gives salt concentration in moles/litre (M)
+            input cannot include units
+        """            
         while True:
-            print("Enter the concentration of salt used. Use \"N/A\" if no salt was used.")         
+            print("Enter the concentration of salt used in moles/litre. Use \"N/A\" if no salt was used. Do not put units.")         
             salt_conc = input()
             if salt_conc == 'q':
                 return 'q'
             elif salt_conc == 'N/A':
-                new_sim.append(salt_conc)
-                break
+                return 'N/A'
             try:
                 salt_conc = float(salt_conc)
             except ValueError:
@@ -231,21 +360,22 @@ class DataSheet(object):
                 if salt_conc <= 0:
                     print("This value should be greater than zero. Please try again.")
                 else:
-                    new_sim.append(salt_conc)
-                    break
-        
+                    return salt_conc
+    
+    def get_comments(self):
+        """ user inputs comments about their simulation
+        """        
         while True:
-            print("Extra comments can be put about the simulation here (i.e. coiled-coil, single helix, parallel, anti-parallel, etc.).")
-            print("Note: there should NOT be any commas in this comment.")
+            print("Extra comments can be put about the simulation here (i.e. coiled-coil, single helix, parallel, anti-parallel, etc.): ")
             comments = input()
             if comments == 'q':
                 return 'q'
-            elif comments.count(',') > 0:
-                print("Do not put any commas in the comments section. Please try again.")
             else:
-                new_sim.append(comments)
-                break
-            
+                return comments
+
+    def get_sim_length(self):
+        """ user provides length of simulation completed in nanoseconds
+        """            
         while True:
             print("Please enter the number of nanoseconds completed so far for this simulation.")
             ns = input()
@@ -259,30 +389,26 @@ class DataSheet(object):
                 if ns < 0:
                     print("Value should be greater than, or equal to, zero. Please try again.")
                 else:
-                    new_sim.append(ns)
-                    break
-
+                    return ns
+    def get_sim_server(self):
+        """ user inputs where the simulation is currently running
+        """
         print("Please enter the name of the server where the simulation is currently running: ")
         server = input()
         if server == 'q':
             return 'q'
         else:
-            new_sim.append(server)
+            return server
         
+    def get_sim_dir(self):
+        """ user inputs where the data for their simulation is stored
+        """        
         print("Please enter the directory path to where the simulation data is stored (i.e. the namd folder): ")
         dir_path = input()
         if dir_path == 'q':
             return 'q'
         else:
-            new_sim.append(dir_path)
-       
-        # check length of list (in case extra commas were added)
-        if len(new_sim) > 11:
-            print("Unable to process request. There was probably an extra commas somewhere in the input. Please try again.")
-        
-        print(new_sim)
-        
-        return new_sim
+            return dir_path
         
 # instantiate class members
 
@@ -305,10 +431,11 @@ if __name__ == "__main__":
         print('', "Options: ", sep='\n')
         print("(1) Make a new simulation record.")
         print("(2) Update a current simulation record in "+sim_data.fname+'.')
-        print("(3) Quit.", '', sep='\n')
+        print("(3) View details about a current simulation record.")
+        print("(4) Quit.", '', sep='\n')
         choice = input()
         print()
-        if choice == '3':
+        if choice == '4':
             break
         elif choice == '2':
             sim_data.print_sim_names()
